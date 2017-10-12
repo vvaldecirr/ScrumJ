7a41001cf71823972a7f828e879617df6ed86c95
Luan Sena <luansena@live.com>
2017-10-11 22:15:55 -0300

    corrige consistencia de ProductBacklog no metodo remove

--- src/model/ProductBacklog.java
+++ src/model/ProductBacklog.java
@@ -7,108 +7,111 @@ import java.util.List;
  * Model Class
  *
  * @author Valdecir
+ *
  */
 public class ProductBacklog {
 
-    private Date deadLine;
-    private Project project;
-    private Professional scrumMaster;
-    private List<UserStory> UserStories;
+	private Date deadLine;
+	private Project project;
+	private Professional scrumMaster;
+	private List<UserStory> listUserStories;
 
-    /**
-     * Simple Constructor
-     */
-    public ProductBacklog() {
+	/**
+	 * Simple Constructor
+	 */
+	public ProductBacklog() {
 
-    }
+	}
 
-    /**
-     * Constructor
-     *
-     * @param deadLine
-     * @param project
-     * @param productOwner
-     */
-    public ProductBacklog(Date deadLine, Project project, Professional scrumMaster) {
-        super();
-        this.setDeadLine(deadLine);
-        this.setProject(project);
-        this.setScrumMaster(scrumMaster);
-    }
+	/**
+	 * Constructor
+	 *
+	 * @param deadLine
+	 * @param project
+	 * @param productOwner
+	 */
+	public ProductBacklog(Date deadLine, Project project, Professional scrumMaster) {
+		super();
+		this.setDeadLine(deadLine);
+		this.setProject(project);
+		this.setScrumMaster(scrumMaster);
+	}
 
-    public Date getDeadLine() {
-        return deadLine;
-    }
+	public Date getDeadLine() {
+		return deadLine;
+	}
 
-    public void setDeadLine(Date deadLine) {
-        this.deadLine = deadLine;
-    }
+	public void setDeadLine(Date deadLine) {
+		this.deadLine = deadLine;
+	}
 
-    public Project getProject() {
-        return project;
-    }
+	public Project getProject() {
+		return project;
+	}
 
-    /**
-     * Método alterado para garantir que a visibilidade bidirecional seja íntegra
-     *
-     * @param productBacklog
-     */
-    public void setProject(Project project) {
+	/**
+	 * Método alterado para garantir que a visibilidade bidirecional seja íntegra
+	 * @param productBacklog
+	 */
+	public void setProject(Project project) {
 //		this.project = project;
 
-        //Se o objeto a ser setado for o mesmo, não fazer nada
-        if (this.project == project)
-            return;
+		//Se o objeto a ser setado for o mesmo, não fazer nada
+		if (this.project == project)
+			return;
 
-        //Se estiver nulificando o atributo garantir a integridade
-        if (project == null) {
-            this.project.setProductBacklog(null);
-            this.project = null;
-        } else { //Se o objeto a ser setado não for o mesmo, substituir garantindo a integridade
-            if (this.project != null)
-                this.project.setProductBacklog(null);
+		//Se estiver nulificando o atributo garantir a integridade
+		if (project == null) {
+			this.project.setProductBacklog(null);
+			this.project = null;
+		} else { //Se o objeto a ser setado não for o mesmo, substituir garantindo a integridade
+			if (this.project != null)
+				this.project.setProductBacklog(null);
 
-            this.project = project;
-            project.setProductBacklog(this);
-        }
-    }
+			this.project = project;
+			project.setProductBacklog(this);
+		}
+	}
 
-    public Professional getScrumMaster() {
-        return scrumMaster;
-    }
+	public Professional getScrumMaster() {
+		return scrumMaster;
+	}
 
-    public void setScrumMaster(Professional scrumMaster) {
-        this.scrumMaster = scrumMaster;
-    }
+	public void setScrumMaster(Professional scrumMaster) {
+		this.scrumMaster = scrumMaster;
+	}
 
-    public List<UserStory> getUserStories() {
-        return UserStories;
-    }
+	public List<UserStory> getUserStories() {
+		return listUserStories;
+	}
 
     public void addUserStory(UserStory userStory) {
-        if (this.UserStories.contains(userStory))
+        if (this.listUserStories.contains(userStory))
             return;
 
         if (userStory == null) {
             return;
         } else {
-            this.UserStories.add(userStory);
+            this.listUserStories.add(userStory);
             userStory.setProductBacklog(this);
         }
     }
 
-    public void removeUserStory(UserStory userStory) {
-        this.UserStories.remove(userStory);
-    }
+	public void removeUserStory(UserStory userStory) {
+        if (this.listUserStories.contains(userStory)){
+            userStory.setProductBacklog(null);
+            this.listUserStories.remove(userStory);
+		}
+	}
 
-    /**
-     * Método que retorna uma String de descrição do objeto
-     */
-    @Override
-    public String toString() {
-        return "\n\nPRODUCTBACKLOG\nData: " + this.getDeadLine()
-                + "\nProjeto: " + this.getProject().getTitle()
-                + "\nScrumMaster: " + this.getScrumMaster();
-    }
+	/**
+	 * Método que retorna uma String de descrição do objeto
+	 */
+	@Override
+	public String toString() {
+		return "\n\nPRODUCTBACKLOG\nData: " + this.getDeadLine()
+				+ "\nProjeto: " + this.getProject().getTitle()
+				+ "\nScrumMaster: " + this.getScrumMaster();
+	}
 
 }
